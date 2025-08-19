cac0e557814927350a0a942e344a127a81995d09
Mahek2710 <2023.mahek.hingorani@ves.ac.in>
2025-08-20 01:45:17 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import connect from './db/db.js';
+import userRoutes from './routes/user.routes.js';
+
+connect();
+
+
+const app = express();
+
+//middleware must come BEFORE routes
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/users', userRoutes);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+export default app;
