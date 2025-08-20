42541e2d21536296fc04fda658803e85ddfa4019
Mahek2710 <2023.mahek.hingorani@ves.ac.in>
2025-08-21 00:47:26 +0530

    Implemented auth middleware and JWT protected routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,22 +2,57 @@ import userModel from '../models/user.model.js';
 import * as userService from '../services/user.service.js';
 import { validationResult } from 'express-validator';
 
-export  const createUserController = async (req, res) => {
-
+// REGISTER
+export const createUserController = async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-
     try {
         const user = await userService.createUser(req.body);
-
         const token = await user.generateJWT();
-        
         res.status(201).json({ user, token });
     } catch (error) {
         return res.status(400).json({ message: error.message });
     }
-}
+};
+
+// LOGIN
+export const loginController = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email }).select('+password');
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isMatch = await user.isValidPassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = await user.generateJWT();
+        res.status(200).json({ user, token });
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+// PROFILE
+export const profileController = async (req, res) => {
+    console.log(req.user);
+
+    res.status(200).json({
+        user: req.user
+    });
+};
